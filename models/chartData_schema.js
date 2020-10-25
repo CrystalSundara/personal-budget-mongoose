5a96865c6d68da30ec82a13048f6f4e4dfac8ccb
Crystal <crystal.sundara@gmail.com>
2020-10-25 16:30:12 -0400

    Added mongoose and chartData model

---
+++ models/chartData_schema.js
@@ -0,0 +1,20 @@
+const mongoose = require ("mongoose");
+
+const chartDataSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    budget: {
+        type: Number,
+        required: true
+    },
+    color: {
+        type: String,
+        required: true,
+        match: [/^#(?:[0-9a-fA-F]{3}){1,2}$/, "Not a valid hex color"]
+    }
+}, {collection: 'chartData'})
+
+module.exports = mongoose.model('chartData', chartDataSchema)
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
+const mongoose = require ("mongoose");
+const chartDataModel = require ("./models/chartData_schema")
+
+const url = 'mongodb://localhost:27017/myBudget'
 const port = 3000;
 
 app.use('/', express.static('public'));
@@ -17,3 +21,78 @@ app.get('/budget', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
+
+
+
+
+
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(()=> {
+            console.log("Connected to the database")
+            // List all entries
+            // chartDataModel.find({})
+            //             .then((data)=>{
+            //                 console.log(data)
+            //                 mongoose.connection.close()
+            //             })
+            //             .catch((connectionError)=>{
+            //                 console.log(connectionError)
+            //             })
+            
+            // // Fetch one document
+            // chartDataModel.find({title: "Eat out"})
+            //             .then((data)=>{
+            //                 console.log(data)
+            //                 mongoose.connection.close()
+            //             })
+            //             .catch((connectionError)=>{
+            //                 console.log(connectionError)
+            //             })
+
+            // Fetch by ID
+            // chartDataModel.findById("5f94a22223175da5a9b91fdc")
+            //             .then((data)=>{
+            //                 console.log(data)
+            //                 mongoose.connection.close()
+            //             })
+            //             .catch((connectionError)=>{
+            //                 console.log(connectionError)
+            //             })
+
+            // Insert data
+            // let newData = new chartDataModel({title: "Testing valid hex", budget: 300, color: "#ABC123"});
+            // chartDataModel.insertMany(newData)
+            //             .then((data)=>{
+            //                 console.log(data)
+            //                 mongoose.connection.close()
+            //             })
+            //             .catch((connectionError)=>{
+            //                 console.log(connectionError)
+            //             })
+
+            // Update data
+            // let newData = {$set: {id: 10, name: "added a new name from MangoDB Native Driver"}};
+            // chartDataModel.update({id: 10}, newData)
+            //             .then((data)=>{
+            //                 console.log(data)
+            //                 mongoose.connection.close()
+            //             })
+            //             .catch((connectionError)=>{
+            //                 console.log(connectionError)
+            //             })       
+            
+            // Delete data
+            chartDataModel.remove({color: "#000000"})
+                        .then((data)=>{
+                            console.log(data)
+                            mongoose.connection.close()
+                        })
+                        .catch((connectionError)=>{
+                            console.log(connectionError)
+                        })     
+
+        })
+        .catch((connectionError) => {
+            console.log(connectionError)
+        })
